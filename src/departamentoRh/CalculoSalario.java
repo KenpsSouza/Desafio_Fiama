9ddb20d0979897aa98994f2622eca4cafe8220e4
KenpsSouza <kenpsrossi@gmail.com>
2025-04-17 20:01:07 -0300

    usando char como variavel

--- src/departamentoRh/CalculoSalario.java
+++ src/departamentoRh/CalculoSalario.java
@@ -6,17 +6,15 @@ public class CalculoSalario {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("  Olá, vamos calcular seu Sálario! ");
 		
-		System.out.println(" Informe o Sálario Bruto: ");
+		System.out.println(" Olá, vamos calcular seu Sálario! --> Informe o Sálario Bruto: ");
 		double salarioBruto = sc.nextDouble();
 		
 		System.out.println(" Informe o número de dependentes: ");
-		int dependentes = sc.nextInt();
-		
+		int dependentes = sc.nextInt();		
 		sc.nextLine();
 			
-		System.out.println("Informe o Plano de Saúde: (B para Básico, A para Avançado ou N para Nenhum): ");
+		System.out.println(" Informe o Plano de Saúde: (B para Básico, A para Avançado ou N para Nenhum): ");
 		char planoSaude = sc.nextLine().toUpperCase().charAt(0);
 		
 		System.out.println(" Usa Vale Transporte? (digite: S para Sim ou N para Não): ");
@@ -28,9 +26,7 @@ public class CalculoSalario {
 		System.out.println(" Usa Vale Refeição? (digite: S para Sim ou N para Não): ");
 		char vRefeicao = sc.nextLine().toUpperCase().charAt(0);
 		
-		
 		double descontoINSS;
-		
 		
 		if (salarioBruto <= 1302.00) {
 			descontoINSS = salarioBruto * 0.08;
@@ -47,8 +43,7 @@ public class CalculoSalario {
 				descontoINSS = 570.88;
 			}	
 		}
-		
-		
+				
 		double baseIR = salarioBruto - (dependentes * 189.59);
 		double descontoIR;
 		
@@ -71,6 +66,7 @@ public class CalculoSalario {
 		double descontoVT = vTransporte == 'S' ?  salarioBruto * 0.06 : 0;
 		double descontoVA = vAlimentacao == 'S' ? 200.0 : 0;
 		double descontoVR = vRefeicao == 'S' ? 250.00 : 0;
+		
 		double descontoPlano = 0;
 		
 		if (planoSaude == 'B') {
@@ -81,29 +77,28 @@ public class CalculoSalario {
 			descontoPlano = 0;
 		} else {
 			System.out.println("Digite uma das opções !");
-		}
-		
+		}	
 		
 		double totalDescontos = descontoINSS + descontoIR + descontoVT + descontoVA + descontoVR + descontoPlano;
 		double salarioLiquido = salarioBruto - totalDescontos;
 		double percentualDesconto = (totalDescontos/salarioBruto) * 100;
+		
+	    System.out.println("\n===============================================");
+		System.out.printf(" Desconto do INSS => R$ %.2f\n",descontoINSS);
+		System.out.printf(" Salario base para IR => R$ %.2f\n" ,baseIR);		
+		System.out.printf(" Desconto do IR => R$ %.2f\n",descontoIR);
+	    System.out.printf(" Desconto do Plano de Saúde => R$ %.2f\n",descontoPlano);
+	    System.out.printf(" Desconto de Vale Transporte => R$ %.2f\n",descontoVT);
+	    System.out.printf(" Desconto de Vale Alimentação => R$ %.2f\n",descontoVA);
+	    System.out.printf(" Desconto de Vale Refeição => R$ %.2f\n",descontoVR);
+	    System.out.println("===============================================");
+	    System.out.println("                 RESUMO FINAL");
+	    System.out.printf("\n Salário bruto => R$ %.2f\n",salarioBruto);
+	    System.out.println("");
+	    System.out.printf(" Percentual de desconto => %.2f%%\n", percentualDesconto);
+	    System.out.println("");
+	    System.out.printf(" Salário liquido => R$ %.2f\n",salarioLiquido);
 	
-		
-		System.out.printf(" Desconto do INSS é: R$ %.2f\n", descontoINSS);
-		System.out.printf(" Salario base para IR é: R$ %.2f\n" , baseIR);		
-		System.out.printf(" Desconto do IR é: R$ %.2f\n", descontoIR);
-	    System.out.printf(" Desconto de Vale Transporte: R$ %.2f\n", descontoVT);
-	    System.out.printf(" Desconto de Vale Alimentação: R$ %.2f\n", descontoVA);
-	    System.out.printf(" Desconto de Vale Refeição: R$ %.2f\n", descontoVR);
-	    System.out.printf(" Desconto do Plano de Saúde: R$ %.2f\n", descontoPlano);
-	    System.out.println("");
-	    System.out.printf(" Percentual de desconto é : %.2f\n", percentualDesconto);
-	    System.out.println("");
-	    System.out.printf(" O Salário liquido é : R$ %.2f\n", salarioLiquido);
-
-		
 		sc.close();
-
 	}
-
 }
